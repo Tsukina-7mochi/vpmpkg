bfef6810ea0e10b64e9fc9bd0567c687df3cfe21
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-10-19 17:56:07 +0900

    refactor: move env check logic to env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,13 @@
+import * as colors from 'fmt/colors.ts';
+
 const apiToken = Deno.env.get('VPMPKG_GH_API_TOKEN');
+
+if (typeof apiToken !== 'string') {
+  console.log(
+    `${
+      colors.yellow('Warning')
+    }: GitHub API Token not found. Using public API.`,
+  );
+}
 
 export { apiToken };
--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,11 @@
 import { Server } from 'http/mod.ts';
-import * as colors from 'fmt/colors.ts';
 import handler from './handler.ts';
-import { apiToken } from './env.ts';
 
 const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = new Server({ hostname, port, handler });
 
-if (typeof apiToken !== 'string') {
-  console.log(
-    `${
-      colors.yellow('Warning')
-    }: GitHub API Token not found. Using public API.`,
-  );
-}
 console.log(`Server started.
   hostname: ${hostname}
   port    : ${port}
