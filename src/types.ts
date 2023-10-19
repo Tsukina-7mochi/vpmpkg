2f6ab9441040776c0943d4d962a28e2e6760f253
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-10-19 16:43:37 +0900

    feat: implement manifest types

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+type UnityPackageManifest = {
+  name: string,
+  version: string,
+  description?: string,
+  displayName?: string,
+  unity?: `${number}.${number}`,
+  author?: {
+    name: string,
+    email?: string,
+    url?: string,
+  },
+  dependencies?: { [key: string]: string },
+  hideInEditor?: boolean,
+  keywords?: string[],
+  license?: string,samples: {
+    displayName: string,
+    description: string,
+    path: string,
+  }[],
+  type?: string,
+  unityRelease?: string,
+};
+
+type VMPPackageManifest = UnityPackageManifest & {
+  url: string,
+  vpmDependencies?: { [key: string]: string },
+  legacyFolders?: { [key: string]: string },
+  legacyFiles?: { [key: string]: string },
+  legacyPackages?: string[],
+};
+
+type VPMRepoManifest = {
+  name: string,
+  id: string,
+  url: string,
+  author: string,
+  packages: {
+    versions: { [key: string]: VMPPackageManifest },
+  },
+};
+
+export type { VMPPackageManifest, VPMRepoManifest };
